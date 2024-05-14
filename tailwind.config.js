47e6a7fdb80e0b427c14f6c3c71bf21395ae58d7
KevinPol12 <kevinpolanco12@gmail.com>
2024-05-14 11:21:34 -0600

    Overring the height screen value from 100vh to 100dvh

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
+    extend: {
+      /*We can see under Tailwinds default configurations that the - height for screen - is "100vh" 
+    The deal with it, is that we should override it to "100dvh" which is 100 dynamic viewport height, because the prior causes issues on mobile devices, so by changing it to dvh, this problem is solved.  */
+      height: {
+        screen: "100dvh",
+      },
+    },
   },
   plugins: [],
 };
